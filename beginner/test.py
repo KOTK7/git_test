6e67a041d6ffba2ea29f1bbb66369ad1c1054dd2
KOTK7 <ahmednasr5032@yahoo.com>
2023-10-19 15:07:02 +0300

    still doing my best

--- beginner/test.py
+++ beginner/test.py
@@ -1,3 +1,9 @@
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-message = f"My first bicycle was a {bicycles[3].title()}."
-print(message)
+age = int(input())
+while True:
+    if age < 3:
+        print("ticket is free")
+    elif age > 3 and age <12:
+        print("the ticket is 10$")
+    elif age > 12:
+        print("the ticket is 15$")
+    break
