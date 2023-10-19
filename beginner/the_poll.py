12a77764b96f26f0a8e1af6eba3db257c4d364da
KOTK7 <ahmednasr5032@yahoo.com>
2023-10-19 16:01:16 +0300

    the poll is coming

--- beginner/the_poll.py
+++ beginner/the_poll.py
@@ -1,10 +1,12 @@
 responses = {}
 polling_active = True
 while polling_active:
-    name = input("what is your name?")
-    response = input("which mountain you want to climb one day?")
+    name = input("what is your name? ")
+    response = input("which mountain you want to climb one day? ")
     responses[name]= response
-    repeat= input("would you like to let another vote?")
+    repeat= input("would you like to let another vote? ")
     if repeat =="no":
         polling_active=False
-    
+print("\n ____poll results_____\n")
+for name,response in responses.items():
+    print(f"{name} would like to climb {response}")
