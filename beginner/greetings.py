09aac02e48f1f3c8b7ffc6e0120369477155d8cb
KOTK7 <ahmednasr5032@yahoo.com>
2023-10-22 00:35:16 +0300

    greetings

--- beginner/greetings.py
+++ beginner/greetings.py
@@ -1,8 +1,8 @@
-def show_messages():
+def send_messages():
     msgs =["Hi","Hello","Awesome"]
     new_msgs =[]
     while msgs:
         dele = msgs.pop()
         print(dele)
         new_msgs.append(dele)
-show_messages()
+send_messages()
