69a3af38f9ee212084649f3d914071625be24812
KOTK7 <ahmednasr5032@yahoo.com>
2023-10-24 02:46:53 +0300

    refactoring code

--- beginner/data.py
+++ beginner/data.py
@@ -1,9 +1,7 @@
 data = {}
-user_email = input("enter your email please : ")
-user_name = input("enter your user name please : ")
-data["email"] = user_email
-data["user name"] = user_name
-print(f"Welcome {user_name}!, your email is {user_email}. \n")
+data["email"] = input("enter your email please : ")
+data["user name"] = input("enter your user name please : ")
+print(f"Welcome {data['user name']}!, your email is {data['email']}. \n")
 print("is it correct?")
 boolean = (input())
 if boolean == "yes":
