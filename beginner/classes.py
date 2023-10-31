d803c883a66aa048a2848bbc00bededf708c660a
KOTK7 <ahmednasr5032@yahoo.com>
2023-10-31 11:14:05 +0200

    this is my dog

--- beginner/classes.py
+++ beginner/classes.py
@@ -1,2 +1,11 @@
 class Dog:
-    pass
+    def __init__(self, name, age):
+        self.name = name
+        self.age= age
+    def sit(self):
+        print(f"{self.name} is sitting")
+    def roll_over(self):
+        print(f"{self.name} rolled over")
+my_dog = Dog("willie",6)
+print(f"my dog's name is {my_dog.name}")
+print(f"my dog's is {my_dog.age} years old")
