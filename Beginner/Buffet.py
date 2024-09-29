50cfe60ac1c72bfa78c7217b27a03953da4e9c48
KOTK7 <ahmednasr5032@proton.me>
2024-09-29 23:25:56 +0300

    opening a restaurant!

---
+++ Beginner/Buffet.py
@@ -0,0 +1,7 @@
+restaurant_menu = ("falafel","ful","kebab","koshari","mahshi")
+for food in restaurant_menu:
+    print(food)
+#** restaurant_menu[0] = "apples" : it produced an error hence commenting, it was meant to do so
+restaurant_menu = ("potatoes","ful","kebab","koshari","3ads")
+for food in restaurant_menu:
+    print(food)
