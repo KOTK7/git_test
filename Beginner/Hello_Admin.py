95c63c267ee0cbb0b317a6021e129d870104833c
KOTK7 <ahmednasr5032@proton.me>
2024-10-01 18:14:29 +0300

    usernames

--- Beginner/Hello_Admin.py
+++ Beginner/Hello_Admin.py
@@ -1,4 +1,4 @@
-users = ["kotk7","plain_cracker77","jadon_banjo45","child_maker66","admin"]
+users = ["hub_player12","plain_cracker77","jadon_banjo45","child_maker66","admin"]
 for user in users:
     if user == "admin":
         print(f"Hello {user}, would you like to see status report?")
