3ea693455daa56b9c9818be5bd8568d98ddb0a8b
KOTK7 <ahmednasr5032@proton.me>
2024-10-01 19:22:49 +0300

    private

---
+++ Beginner/Person.py
@@ -0,0 +1,2 @@
+person = {"first_name":"Jack","last_name":"jones","age":"27","city":"NYC"}
+print(f"Hi my name is {person['first_name']} {person['last_name']}, My age is {person['age']} and I live in {person['city']}")
