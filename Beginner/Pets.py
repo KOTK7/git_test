98f509ec27f3163019d2061bb73c7274be42bcf0
KOTK7 <ahmednasr5032@proton.me>
2024-10-01 21:46:50 +0300

    i love dogs

--- Beginner/Pets.py
+++ Beginner/Pets.py
@@ -3,5 +3,6 @@ pets = {
     "goldy":{"kind":"fish","owner name":"william"},
     "max":{"kind":"dog","owner name":"jake"}
 }
+pets["lolo"] = {"kind":"dog","owner name":"sasha"}
 for name, info in pets.items():
     print(f"{name} is a {info['kind']} and its owner's name is {info['owner name']}")
