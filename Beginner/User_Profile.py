911fed44b9c0c097c98dd474bcf3ee24088fa0e4
KOTK7 <ahmednasr5032@proton.me>
2024-11-20 07:03:30 +0200

    Now that looks better

--- Beginner/User_Profile.py
+++ Beginner/User_Profile.py
@@ -2,6 +2,7 @@ def build_profile(first, last, **user_info):
     user_info["first name"] = first
     user_info["last name"] = last
     return user_info
+
 user_profile = build_profile(
     "Jack",
     "Johns",
