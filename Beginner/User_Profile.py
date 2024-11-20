d4919a87d171b1c2fa4bba06b127bf06b970c89d
KOTK7 <ahmednasr5032@proton.me>
2024-11-20 06:55:20 +0200

    redesigning my files

--- Beginner/User_Profile.py
+++ Beginner/User_Profile.py
@@ -3,4 +3,4 @@ def build_profile(first, last, **user_info):
     user_info["last name"] = last
     return user_info
 user_profile = build_profile("Jack","Johns", Status = "single", Student = "Nope")
-print(user_profile)
+print(f"Hi my name is: {user_profile['first name']} {user_profile['last name']}, I'm {user_profile['Status']} and i'm not a Student")
