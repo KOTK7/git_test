0bb17ffbae28748a995dbcf91f25c1fc98254621
KOTK7 <ahmednasr5032@proton.me>
2025-04-07 15:59:16 +0200

    what do you think?

---
+++ Beginner/Pets.py
@@ -0,0 +1,4 @@
+pets = ["dog", "cat", "bird"]
+for pet in pets:
+    print(f'A {pet} would make a great pet')
+print("Any of these would make a great pet")
---
+++ Beginner/Pizzas.py
@@ -0,0 +1,4 @@
+pizzas = ["cheese", "pepperoni", "mushroom"]
+for pizza in pizzas:
+    print(f'I like {pizza} pizza')
+print("I really Love Pizza!")
