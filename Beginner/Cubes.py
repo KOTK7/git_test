b538f864d6e0f8ef414143df0ebf69f832e214a9
KOTK7 <ahmednasr5032@proton.me>
2025-04-07 16:20:47 +0200

    doing some math

---
+++ Beginner/Cubes.py
@@ -0,0 +1,6 @@
+cubes = []
+for n in range(1,11):
+    cubed = n**3
+    cubes.append(cubed)
+print(cubes)
+print([x**3 for x in range(1, 11)])
---
+++ Beginner/numbers.py
@@ -0,0 +1,9 @@
+numbers = [x for x in range(1,21)]
+counting = numbers
+print(counting)
+# million = [x for x in range(1, 1_000_001)]
+# print(million)
+# for odd numbers
+print([x for x in range(1,21) if x%2 != 0])
+# for even numbers
+print([x for x in range(1,21) if x%2 == 0])
