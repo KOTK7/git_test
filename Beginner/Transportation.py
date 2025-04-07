d47812c4e3ba17c3195df510050a0b92d99d335e
KOTK7 <ahmednasr5032@proton.me>
2025-04-07 14:55:03 +0200

    moving them

---
+++ Beginner/Transportation.py
@@ -0,0 +1,4 @@
+car = ['Tesla','Bugatti','BYD']
+print(f'my best way of transportation is {car[0]}')
+print(f'my best way of transportation is {car[1]}')
+print(f'my best way of transportation is {car[2]}')
---
+++ Beginner/names.py
@@ -0,0 +1,5 @@
+names = ['jack','bobo','mark']
+print(f'my friend is {names[0].title()}')
+print(f'my friend is {names[1].title()}')
+print(f'my friend is {names[2].title()}')
+#KOTK7 7th of April 2025
