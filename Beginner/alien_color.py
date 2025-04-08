d33bffffe0ec58497864265754d6dfb3367aff34
KOTK7 <ahmednasr5032@proton.me>
2025-04-08 18:38:53 +0200

    alien invasion

---
+++ Beginner/alien_color.py
@@ -0,0 +1,7 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print('you earned 5 points')
+elif alien_color == 'yellow':
+    print('you earned 10 points')
+else:
+    print('you earned 15 points')
