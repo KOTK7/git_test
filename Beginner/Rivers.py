65de6eef24894861944b29fed75c847bb16c424a
KOTK7 <ahmednasr5032@proton.me>
2025-04-10 19:43:00 +0200

    rivers across the world

---
+++ Beginner/Rivers.py
@@ -0,0 +1,8 @@
+rivers = {'Nile': 'Egypt', 'Yangtze': 'China', 'Amazon River': 'Amazon'}
+for key, value in rivers.items():
+    print(f'The {key} is in {value}')
+    print(key)
+    print(value)
+# = 1
+# for y in range(30):
+# print(x+y)
