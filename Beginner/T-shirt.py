6ea16cc056ec5bb415b2057e14e6d4aedba096da
KOTK7 <ahmednasr5032@proton.me>
2025-04-11 21:09:10 +0200

    T-shirt size

---
+++ Beginner/T-shirt.py
@@ -0,0 +1,5 @@
+def make_shirt(text='I love Python', size='L'):
+    print(f'the shirt size is {size} and the text to be written is {text}')
+make_shirt('Awesome', "S")
+make_shirt(size='M', text="Great")
+make_shirt()
