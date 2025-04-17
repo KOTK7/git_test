3bd5ca5d134d0340124507f75bf905bd57a1fd83
KOTK7 <ahmednasr5032@proton.me>
2025-04-17 21:12:59 +0200

    opened a restaurnat

---
+++ Beginner/Class.py
@@ -0,0 +1,18 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        print('the dog is sitting')
+    
+    def roll_over(self):
+        print('the dog is rolling over')
+    
+    def who(self):
+        print(f"The dog's name is {self.name}, and he is {self.age} years old.")
+
+
+dog1 = Dog('Max', '4')
+
+dog1.who()
---
+++ Beginner/Restaurant.py
@@ -0,0 +1,16 @@
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.name = restaurant_name
+        self.cuisine = cuisine_type
+    
+    def describe_restaurant(self):
+        print(f"The restaurant name is {self.name} and its cuisine is  {self.cuisine}\n")
+
+    def open_restaurant(self):
+        print('the restaurant is open')
+
+restaurant1 = Restaurant('Mariano', 'Italian')
+
+restaurant1.describe_restaurant()
+
+restaurant1.open_restaurant()
