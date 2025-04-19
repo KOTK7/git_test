5a895f88685888ed4890f90e09a95f359ed5b5b6
KOTK7 <ahmednasr5032@proton.me>
2025-04-19 15:35:49 +0200

    added login attempts

--- Beginner/User.py
+++ Beginner/User.py
@@ -3,14 +3,32 @@ class User:
         self.f_name = first_name
         self.l_name = last_name
         self.username = user_name
+        self.login_attempts = 0
 
     def describe_user(self):
-        print(f"the user's first name is {self.f_name}")
-        print(f"and his last name is {self.l_name} and his username is {self.username}")
+        print(f"The user's first name is {self.f_name}")
+        print(f"And their last name is {self.l_name} and their username is {self.username}")
     
     def greet_user(self):
         print(f"Hello {self.username}, it is nice to see you back!")
 
-user1 = User('Jack','Paul','jakey22')
+    def increment_login_attempts(self, no_of_logins=1):
+        self.login_attempts += no_of_logins
+        print(f"{no_of_logins} login attempt(s) added. Total login attempts now: {self.login_attempts}")
+
+    def show_attempts(self):
+        print(f"{self.username}'s login attempts: {self.login_attempts}")
+
+    def reset_login_attempts(self):
+        print(f"Resetting login attempts for {self.username}. Previous attempts: {self.login_attempts}")
+        self.login_attempts = 0
+
+user1 = User('Jack', 'Paul', "Jackey22")
+
 user1.describe_user()
 user1.greet_user()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.show_attempts()
+user1.reset_login_attempts()
